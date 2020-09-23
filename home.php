4ef2eef3787fef5b7a2812792855b06b60bf3204
Guillaume Perbet <perbet.dev@gmail.com>
2020-09-23 15:36:57 +0200

    style post image

--- home.php
+++ home.php
@@ -23,39 +23,51 @@
         </div>
     </nav>
 
-    <!-- wp loop -->
-    <?php if( have_posts() ){ while( have_posts() ){ the_post(); ?>
-
-        <article>
-
-            <!-- title -->
-            <h2>
-                <?php the_title(); ?>
-            </h2>
+    <section id="blog">
+        <div class="container">
+            <div class="row">
+                <!-- wp loop -->
+                <?php if( have_posts() ){ while( have_posts() ){ the_post(); ?>
         
-            <!-- image -->
-            <?php if ( has_post_thumbnail() ){ ?>
-                <div>
-                    <?php the_post_thumbnail(); ?>
+                <div class="col-12 col-md-6 col-xl-4">
+                    <article>
+            
+                        <!-- image -->
+                        <?php if ( has_post_thumbnail() ){ ?>
+                            <div class="image">
+                                <?php the_post_thumbnail(); ?>
+                                <p class="tag"><?php the_tags(''); ?></p>
+                            </div>
+                        <?php } ?>
+
+                        <!-- title -->
+                        <h2>
+                            <?php the_title(); ?>
+                        </h2>
+            
+                        
+                        
+                        <!-- content -->
+                        <p>
+                            Publié le <?php the_time( get_option( 'date_format' ) ); ?> 
+                            par <?php the_author(); ?> • <?php comments_number(); ?>
+                        </p>
+                        
+                        <p>
+                            <?php the_excerpt(); ?>
+                        </p>
+            
+                        <p>
+                            <a href="<?php the_permalink(); ?>" class="post__link">Lire la suite</a>
+                        </p>
+                    </article>
                 </div>
-            <?php } ?>
-            
-            <!-- content -->
-            <p>
-                Publié le <?php the_time( get_option( 'date_format' ) ); ?> 
-                par <?php the_author(); ?> • <?php comments_number(); ?>
-            </p>
-            
-            <p>
-                <?php the_excerpt(); ?>
-            </p>
+        
+                <?php }} ?>
+            </div>
+        </div>
 
-            <p>
-                <a href="<?php the_permalink(); ?>" class="post__link">Lire la suite</a>
-            </p>
-        </article>
-
-    <?php }} ?>
+    </section>
 
 </main>
 
