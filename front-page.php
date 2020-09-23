f62ec2d0b45cf57adc245735635b0dc823716ef9
Guillaume Perbet <perbet.dev@gmail.com>
2020-09-23 12:23:50 +0200

    style front page content

--- front-page.php
+++ front-page.php
@@ -1,12 +1,25 @@
 <?php get_header(); ?>
 
-<!-- logo -->
-<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="logo avec le texte 'ça marche' en dessous d'un dessin de montagne" width="200" height="200">
-
-<!-- wp loop -->
-<?php if( have_posts() ){ while( have_posts() ){ the_post(); ?>
-    <!-- title from wp -->
-    <h1><?php the_title(); ?></h1>
-<?php }} ?>
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col-8 offset-2 col-sm-6 offset-sm-3 col-md-4 offset-md-0">
+                <!-- QUERY LOGO FROM WP ADMIN -->
+                <p>
+                    <img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="logo avec le texte 'ça marche' en dessous d'un dessin de montagne" width="200" height="200">
+                </p>
+            </div>
+        
+            <div class="col-10 offset-1 col-sm-8 offset-sm-2 offset-md-0">
+                <h1>
+                    <span>mes</span>
+                    <span>plus</span>
+                    <span>belles</span>
+                    <span>randos</span>
+                </h1>
+            </div>
+        </div>
+    </div>
+</main>
 
 <?php get_footer(); ?>
