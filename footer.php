4da649231a4c539ce113c67e4e85e60f8bf83c1b
Guillaume Perbet <perbet.dev@gmail.com>
2020-09-23 10:49:42 +0200

    style responsive footer

--- footer.php
+++ footer.php
@@ -13,22 +13,22 @@
                 <!-- author -->
                 <div class="col-4">
                     <p><i class="fas fa-feather-alt"></i> Guillaume Perbet</p>
-                    <p><a href="https://guillaumeperbet.fr" target="_blank" rel="noopener noreferrer">www.guillaumeperbet.fr</a></p>
-                    <p><i id="github" class="fab fa-github-square"></i><i id="linkedin" class="fab fa-linkedin"></i></p>
+                    <p class="display-md"><a href="https://guillaumeperbet.fr" target="_blank" rel="noopener noreferrer">www.guillaumeperbet.fr</a></p>
+                    <p class="display-md"><i id="github" class="fab fa-github-square"></i> &nbsp; <i id="linkedin" class="fab fa-linkedin"></i></p>
                 </div>
 
                 <!-- context -->
-                <div class="col-4">
+                <div class="col-4 display-md">
                     <p><i class="fas fa-graduation-cap"></i> Access Code School</p>
-                    <p><i class="fas fa-map-pin"></i> Besançon</p>
-                    <p><i class="fas fa-calendar-alt"></i> Septembre 2020</p>
+                    <p><i class="fas fa-map-pin"></i> &nbsp;&nbsp; Besançon</p>
+                    <p><i class="fas fa-calendar-alt"></i> &nbsp; Septembre 2020</p>
                 </div>
 
                 <!-- terms -->
                 <div class="col-4 col-md-2">
                     <p class="text-center">Politique de confidentialité</p>
                 </div>
-                
+
             </div>
         </div>
     </footer>
