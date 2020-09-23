b32518d3e2808e2223c6bfeb91eca8ad638dbd36
Guillaume Perbet <perbet.dev@gmail.com>
2020-09-23 14:42:05 +0200

    style category nav

--- home.php
+++ home.php
@@ -1,52 +1,62 @@
 <?php get_header(); ?>
 
-<!-- category menu -->
-<!-- ajouter le bouton actif si on est dans une page catégorie is_category()? -->
-<nav>
-    <?php
-        if (has_nav_menu('category')){
-            wp_nav_menu(
-                array(
-                    'theme_location' => 'category',
-                    'container' => 'ul'
-                )
-            );
-        }
-    ?>
-</nav>
+<main>
 
-<!-- wp loop -->
-<?php if( have_posts() ){ while( have_posts() ){ the_post(); ?>
-
-    <article>
-
-        <!-- title -->
-        <h2>
-            <?php the_title(); ?>
-        </h2>
-    
-        <!-- image -->
-        <?php if ( has_post_thumbnail() ){ ?>
-            <div>
-                <?php the_post_thumbnail(); ?>
+    <!-- category menu -->
+    <!-- ajouter le bouton actif si on est dans une page catégorie is_category()? -->
+    <nav id="category">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <?php
+                        if (has_nav_menu('category')){
+                            wp_nav_menu(
+                                array(
+                                    'theme_location' => 'category',
+                                    'container' => 'ul'
+                                )
+                            );
+                        }
+                    ?>
+                </div>
             </div>
-        <?php } ?>
-        
-        <!-- content -->
-        <p>
-            Publié le <?php the_time( get_option( 'date_format' ) ); ?> 
-            par <?php the_author(); ?> • <?php comments_number(); ?>
-        </p>
-        
-        <p>
-            <?php the_excerpt(); ?>
-        </p>
+        </div>
+    </nav>
 
-        <p>
-            <a href="<?php the_permalink(); ?>" class="post__link">Lire la suite</a>
-        </p>
-    </article>
+    <!-- wp loop -->
+    <?php if( have_posts() ){ while( have_posts() ){ the_post(); ?>
 
-<?php }} ?>
+        <article>
+
+            <!-- title -->
+            <h2>
+                <?php the_title(); ?>
+            </h2>
+        
+            <!-- image -->
+            <?php if ( has_post_thumbnail() ){ ?>
+                <div>
+                    <?php the_post_thumbnail(); ?>
+                </div>
+            <?php } ?>
+            
+            <!-- content -->
+            <p>
+                Publié le <?php the_time( get_option( 'date_format' ) ); ?> 
+                par <?php the_author(); ?> • <?php comments_number(); ?>
+            </p>
+            
+            <p>
+                <?php the_excerpt(); ?>
+            </p>
+
+            <p>
+                <a href="<?php the_permalink(); ?>" class="post__link">Lire la suite</a>
+            </p>
+        </article>
+
+    <?php }} ?>
+
+</main>
 
 <?php get_footer(); ?>
