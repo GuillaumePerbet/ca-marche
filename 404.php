1cace310db46ee0092d8fe5021ffa1d948f43108
Guillaume Perbet <perbet.dev@gmail.com>
2020-09-24 16:20:25 +0200

    fix 404 style

--- 404.php
+++ 404.php
@@ -1,12 +1,13 @@
 <?php get_header(); ?>
 
+<main id="page404">
     <div class="container">
 
         <!-- logo with home link -->
         <div class="row">
             <div class="col-12 col-md-8 offset-md-2 col-xl-4 offset-xl-4">
-                <a class="logo" href="<?php echo home_url( '/' ); ?>">
-                    <img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="logo avec le texte 'ça marche' en dessous d'un dessin de montagne" width="200" height="200">
+                <a href="<?php echo home_url( '/' ); ?>">
+                    <img class="size-image" src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="logo avec le texte 'ça marche' en dessous d'un dessin de montagne" width="200" height="200">
                 </a>
             </div>
         </div>
@@ -14,11 +15,12 @@
         <!-- text -->
         <div class="row">
             <div class="col-12 col-xl-8 offset-xl-2">
-                <h1 id="title404">SENTIER NON BALISÉ</h1>
-                <p id="text404">La page demandée n’existe pas</p>
+                <h1>SENTIER NON BALISÉ</h1>
+                <p class="text">La page demandée n’existe pas</p>
             </div>
         </div>
 
     </div>
+</main>
 
 <?php get_footer(); ?>
