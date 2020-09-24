03b1c035f1873c9c6a3fc55988c9152f28600613
Guillaume Perbet <perbet.dev@gmail.com>
2020-09-24 11:27:55 +0200

    style technic and content sections

--- single.php
+++ single.php
@@ -33,23 +33,51 @@
             <div class="row">
                 <div class="col">
                     <!-- excerpt -->
-                    <p class="text">
+                    <div class="text">
                         <?php the_excerpt(); ?>
-                    </p>
+                    </div>
                 </div>
             </div>
-    
-            <!-- content -->
-            <p>
-                Publié le <?php the_date(); ?>
-                par <?php the_author(); ?>
-                Dans la catégorie <?php the_category(); ?>
-                Avec les étiquettes <?php the_tags(); ?>
-            </p>
-    
-            <p>
-                <?php the_content(); ?>
-            </p>
+            
+            <!-- technic -->
+            <div class="row">
+
+                <!-- title -->
+                <div class="col-12">
+                    <h3>
+                        FICHE TECHNIQUE
+                    </h3>
+                </div>
+
+                <!-- description -->
+                <div class="col-4 col-xl-2 offset-xl-4">
+                    <p class="text technic"><i class="fas fa-clock"></i> 2 jours</p>
+                </div>
+
+                <div class="col-4 col-xl-2">
+                    <p class="text technic"><i class="fas fa-mountain"></i> 830m</p>
+                </div>
+
+                <div class="col-4 col-xl-8 offset-xl-2 map">
+                    <p class="text technic display-none-xl"><i class="fas fa-map-marked-alt"></i> carte</p>
+                    <p class="display-xl"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/map.jpg" alt="itinéraire sur une carte"></p>
+                </div>
+
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <!-- content -->
+                    <h3>
+                        RÉCIT COMPLET
+                    </h3>
+
+                    <div class="text">
+                        <?php the_content(); ?>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </article>
     <?php }} ?>
