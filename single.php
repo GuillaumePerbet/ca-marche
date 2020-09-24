0bacf3b07361521aba626f4dbfa1228acb0c5a10
Guillaume Perbet <perbet.dev@gmail.com>
2020-09-24 10:16:49 +0200

    single page hero and title style

--- single.php
+++ single.php
@@ -1,34 +1,49 @@
 <?php get_header(); ?>
 
-<?php if( have_posts() ){ while( have_posts() ){ the_post(); ?>
+<main id="single">
 
-    <article>
+    <?php if( have_posts() ){ while( have_posts() ){ the_post(); ?>
+    <article >
 
         <!-- image -->
         <?php if ( has_post_thumbnail() ){ ?>
-            <div>
+            <div class="image">
                 <?php the_post_thumbnail(); ?>
             </div>
         <?php } ?>
 
-        <!-- title -->
-        <h1>
-            <?php the_title(); ?>
-        </h1>
+        <div id="category">
+            <!-- tag -->
+            <p><?php the_tags(''); ?></p>
 
-        <!-- content -->
-        <p>
-            Publié le <?php the_date(); ?>
-            par <?php the_author(); ?>
-            Dans la catégorie <?php the_category(); ?>
-            Avec les étiquettes <?php the_tags(); ?>
-        </p>
+            <!-- category -->
+            <p><?php the_category(' '); ?></p>
+        </div>
 
-        <p>
-            <?php the_content(); ?>
-        </p>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <!-- title -->
+                    <h2>
+                        <?php the_title(); ?>
+                    </h2>
+                </div>
+            </div>
+    
+            <!-- content -->
+            <p>
+                Publié le <?php the_date(); ?>
+                par <?php the_author(); ?>
+                Dans la catégorie <?php the_category(); ?>
+                Avec les étiquettes <?php the_tags(); ?>
+            </p>
+    
+            <p>
+                <?php the_content(); ?>
+            </p>
+        </div>
     </article>
-
-<?php }} ?>
+    <?php }} ?>
+</main>
 
 <?php get_footer(); ?>
