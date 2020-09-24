ffcfe08eafb3a3b7110a69eb48d7739531d19c1c
Guillaume Perbet <perbet.dev@gmail.com>
2020-09-24 12:00:28 +0200

    style footer

--- footer.php
+++ footer.php
@@ -7,26 +7,34 @@
 
                 <!-- logo -->
                 <div class="col-4 col-md-2">
-                    <img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="logo avec le texte 'ça marche' en dessous d'un dessin de montagne" width="200" height="200">
+                   <div class="footer-items">
+                       <img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="logo avec le texte 'ça marche' en dessous d'un dessin de montagne" width="200" height="200">
+                    </div>
                 </div>
 
                 <!-- author -->
                 <div class="col-4">
-                    <p><i class="fas fa-feather-alt"></i> Guillaume Perbet</p>
-                    <p class="display-md"><a href="https://guillaumeperbet.fr" target="_blank" rel="noopener noreferrer">www.guillaumeperbet.fr</a></p>
-                    <p class="display-md"><i id="github" class="fab fa-github-square"></i> &nbsp; <i id="linkedin" class="fab fa-linkedin"></i></p>
+                    <div class="footer-items">
+                        <p><i class="fas fa-feather-alt"></i> Guillaume Perbet</p>
+                        <p class="display-md"><a href="https://guillaumeperbet.fr" target="_blank" rel="noopener noreferrer">www.guillaumeperbet.fr</a></p>
+                        <p class="display-md"><i id="github" class="fab fa-github-square"></i> &nbsp; <i id="linkedin" class="fab fa-linkedin"></i></p>
+                    </div>
                 </div>
 
                 <!-- context -->
                 <div class="col-4 display-md">
-                    <p><i class="fas fa-graduation-cap"></i> Access Code School</p>
-                    <p><i class="fas fa-map-pin"></i> &nbsp;&nbsp; Besançon</p>
-                    <p><i class="fas fa-calendar-alt"></i> &nbsp; Septembre 2020</p>
+                    <div class="footer-items">
+                        <p><i class="fas fa-graduation-cap"></i> Access Code School</p>
+                        <p><i class="fas fa-map-pin"></i> &nbsp;&nbsp; Besançon</p>
+                        <p><i class="fas fa-calendar-alt"></i> &nbsp; Septembre 2020</p>
+                    </div>
                 </div>
 
                 <!-- terms -->
                 <div class="col-4 col-md-2">
-                    <p id="terms">Politique de confidentialité</p>
+                    <div class="footer-items">
+                        <p>Politique de confidentialité</p>
+                    </div>
                 </div>
 
             </div>
