b8cde43fa3c438dac44b7622ffbf398db2d35443
Guillaume Perbet <perbet.dev@gmail.com>
2020-09-24 13:44:27 +0200

    replace h1 by div

--- page.php
+++ page.php
@@ -4,9 +4,7 @@
     <?php if( have_posts() ){ while( have_posts() ){ the_post(); ?>
         <h1><?php the_title(); ?></h1>
 
-        <div>
-        <h1><?php the_content(); ?></h1>
-        </div>
+        <div><?php the_content(); ?></div>
     <?php }} ?>
 </main>
 
