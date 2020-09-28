e8c0d69bb6f60b97f32fa3c7623be4b5de577842
Guillaume Perbet <perbet.dev@gmail.com>
2020-09-28 10:21:53 +0200

    style terms and conditions page

--- page.php
+++ page.php
@@ -1,11 +1,17 @@
 <?php get_header(); ?>
 
-<main>
-    <?php if( have_posts() ){ while( have_posts() ){ the_post(); ?>
-        <h1><?php the_title(); ?></h1>
-
-        <div><?php the_content(); ?></div>
-    <?php }} ?>
+<main id="terms">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <?php if( have_posts() ){ while( have_posts() ){ the_post(); ?>
+                    <h1><?php the_title(); ?></h1>
+            
+                    <div><?php the_content(); ?></div>
+                <?php }} ?>
+            </dic>
+        </div>
+    </div>
 </main>
 
 <?php get_footer(); ?>
